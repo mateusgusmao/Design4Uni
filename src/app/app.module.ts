9d28aa0570fdebe398755c7ea38b57d01c8ea298
Mateus Gusmão <teumarques14@gmail.com>
2018-12-03 13:52:56 -0300

    Novos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { PacienteComponent } from './paciente/paciente.component';
+import { ConsultasComponent } from './consultas/consultas.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    PacienteComponent,
+    ConsultasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
